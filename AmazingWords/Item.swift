d088975225554f3b72040705a01eaccfabfbe5b5
Shay <ShayJbooking@gmail.com>
2025-02-03 16:47:07 -0500

    Initial Commit

---
+++ AmazingWords/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  AmazingWords
+//
+//  Created by Sharnese Johnson on 2/3/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
